6e21142835a75a3a8111c3fd82910eed1c47307c
printfalling <printfall@yahoo.com>
2016-03-10 18:44:29 +0800

    Revert "Add"
    This reverts commit 5a647c58287c6aec018970e2e5c2ed2d4f0a54c7.

--- Traning.m
+++ Traning.m
@@ -2,7 +2,7 @@
 %% Initialization
 clear ; close all; clc
 Training_Data = '.\datas\ex4data1.mat';
-Save_Theta = '.\datas\Theta.txt'
+Save_Theta = '.\datas\Theta.mat'
 Times_of_tianing = 50;
 lambda = 1;
 
